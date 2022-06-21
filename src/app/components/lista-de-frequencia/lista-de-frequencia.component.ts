4cdff2315bd011d25ba2c9061438054c74cc30c7
Vinícius Oliveira de Carvalho <viniolicar2004@gmail.com>
2022-06-21 15:54:04 -0300

    correção na lista de frequência

--- src/app/components/lista-de-frequencia/lista-de-frequencia.component.ts
+++ src/app/components/lista-de-frequencia/lista-de-frequencia.component.ts
@@ -1,4 +1,3 @@
-import { map, Observable, startWith } from 'rxjs';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -15,24 +14,11 @@ export class ListaDeFrequenciaComponent implements OnInit {
   id:any = []
   alunos = []
   displayedColumns = ['numeroDoAluno', 'nomes', '1', '2', '3', '4', '5', '6'] 
-  frequencia:any = []
   horario:any
   form:any
   frequencias:any = []
 
-  //Auto complete
-  input = new FormControl('', [Validators.required])
-  filterValue: Observable<string[]>;
-  materias:any= ['Portugues', 'Matematica', 'Historia', 'Ingles', 'Geografia']
-
-  constructor(private db:FirebaseService, private turmaId:ActivatedRoute) { 
-    this.filterValue = this.input.valueChanges.pipe(
-      startWith(null),
-      map((materia: string | null) => (materia ? this._filter(materia) : this.materias.slice())),
-    );
-  }
-
-  //Tentar não deixar mandar duas faltas pelo id
+  constructor(private db:FirebaseService, private turmaId:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.createForm()
@@ -43,13 +29,13 @@ export class ListaDeFrequenciaComponent implements OnInit {
       this.getTurmaName(this.pathId)
     })
 
-   this.horarios()
+  //  this.horarios()
   }
 
   createForm() {
     this.form = new FormGroup({
       nomeDoProfessor: new FormControl('', [Validators.required]),
-      materia: this.input
+      materia: new FormControl('', [Validators.required])
     })
   }
 
@@ -96,7 +82,7 @@ export class ListaDeFrequenciaComponent implements OnInit {
           this.db.handleError(error)
         }
       })   
-    } 
+  } 
 
   // Vai pegar os valores do radio button
   presenca(nome:any, value:any) {
@@ -105,52 +91,40 @@ export class ListaDeFrequenciaComponent implements OnInit {
     const month = date.getMonth() + 1
     const year = date.getFullYear()
 
-    this.db.readAlunos(this.pathId).subscribe((res:any) => {
-
-      const ids = res.docs
-      const names = res.docs.map((res:any) => {
-        return res.data().nome
-      })
-
-      const index = names.indexOf(nome)
-      const id = Math.random().toString(12);
-      const frequencia = {
-        id: id,
-        date: `${day}/${month}/${year}`,
-        horario: 1,
-        presenca: value.value,
-        materia: this.form.value.materia,
-        professor: this.form.value.nomeDoProfessor,
-        status: 'Não justificada'
-      }
-    
-      const indexs = this.frequencias.findIndex((age:any) => age.id == frequencia.id);
-    
-    if(indexs == -1) {
-      this.frequencias.push(frequencia)
-    } else if(indexs >= 0) {
-      this.frequencias[indexs] = frequencia
-    } else { return }
-  })
-}
+    const frequencia = {
+      nome: nome,
+      date: `${day}/${month}/${year}`,
+      horario: this.horario,
+      presenca: value.value,
+      materia: this.form.value.materia,
+      professor: this.form.value.nomeDoProfessor,
+      status: 'Não justificada'
+    }
   
- //Vai adicionar a presença
+    const index = this.frequencias.findIndex((freq:any) => freq.nome == frequencia.nome);
+  
+    if(index == -1) {
+      this.frequencias.push(frequencia)
+    } else if(index >= 0) {
+      this.frequencias[index] = frequencia
+    } else { return }
+  }
+  
   addPresenca() {
-  //Vai adicionar presença para cada item no Array de ids
     for (let i = 0; i < this.id.length; i++) {
       this.db.addPresenca(this.pathId, this.id[i], this.frequencias[i])
     }
   }
 
   horarios() {
+    
     this.db.getTurma(this.pathId).subscribe((res:any) => {
       const turno = res.data().turno
       const date = new Date()
       const hour = date.getHours()
       const minutes = date.getMinutes()
       
-    //Horarios que a chamada vai estar aberta
-    //Matutino
+    // Matutino
    if(turno == 'matutino') {
       if(hour == 12 && minutes >= 0 && minutes <= 59) {
         this.horario = 1
@@ -168,47 +142,39 @@ export class ListaDeFrequenciaComponent implements OnInit {
 
     } else if(turno == 'verspetino') {
 
-    //Verspetino
-      // if(hour == 8 && minutes >= 20 && minutes <= 45) {
-      //   this.horario = 1
-      // } else if(hour == 4 && minutes >= 0 && minutes < 10) {
-      //   this.horario = 2
-      // } else if(hour == 16 && minutes >= 0 && minutes < 59) {
-      //   this.horario = 3
-      // } else if(hour == 4 && minutes >= 0 && minutes < 10) {
-      //   this.horario = 4
-      // } else if(hour == 4 && minutes >= 0 && minutes < 10) {
-      //   this.horario = 5
-      // } else if(hour == 4 && minutes >= 0 && minutes < 10) {
-      //   this.horario = 6
-      // }
+    // Verspetino
+      if(hour == 8 && minutes >= 20 && minutes <= 45) {
+        this.horario = 1
+      } else if(hour == 4 && minutes >= 0 && minutes < 10) {
+        this.horario = 2
+      } else if(hour == 16 && minutes >= 0 && minutes < 59) {
+        this.horario = 3
+      } else if(hour == 4 && minutes >= 0 && minutes < 10) {
+        this.horario = 4
+      } else if(hour == 4 && minutes >= 0 && minutes < 10) {
+        this.horario = 5
+      } else if(hour == 4 && minutes >= 0 && minutes < 10) {
+        this.horario = 6
+      }
       
     } else if(turno == 'noturno') {
 
-    //Noturno
-    // if(hour == 8 && minutes >= 20 && minutes <= 45) {
-    //   this.horario = 1
-    // } else if(hour == 4 && minutes >= 0 && minutes < 10) {
-    //   this.horario = 2
-    // } else if(hour == 16 && minutes >= 0 && minutes < 59) {
-    //   this.horario = 3
-    // } else if(hour == 4 && minutes >= 0 && minutes < 10) {
-    //   this.horario = 4
-    // } else if(hour == 4 && minutes >= 0 && minutes < 10) {
-    //   this.horario = 5
-    // } else if(hour == 4 && minutes >= 0 && minutes < 10) {
-    //   this.horario = 6
-    // }
+    // Noturno
+    if(hour == 8 && minutes >= 20 && minutes <= 45) {
+      this.horario = 1
+    } else if(hour == 4 && minutes >= 0 && minutes < 10) {
+      this.horario = 2
+    } else if(hour == 16 && minutes >= 0 && minutes < 59) {
+      this.horario = 3
+    } else if(hour == 4 && minutes >= 0 && minutes < 10) {
+      this.horario = 4
+    } else if(hour == 4 && minutes >= 0 && minutes < 10) {
+      this.horario = 5
+    } else if(hour == 4 && minutes >= 0 && minutes < 10) {
+      this.horario = 6
+    }
     }
   })
-}
-  
-  private _filter(value:any): string[] {
-    const filterValue = value.toLowerCase()
-
-    return this.materias.filter((turma:any) => {
-      turma.toLowerCase().includes(filterValue)
-    })
   }
-
+  
 }
