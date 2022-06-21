9151812c79db485eb1bdc7a23ac6a72a91c25068
Vinícius Oliveira de Carvalho <viniolicar2004@gmail.com>
2022-06-21 09:57:15 -0300

    Add a gitIgnore

--- src/environments/env.ts
+++ src/environments/env.ts
@@ -1,9 +1,8 @@
 export const firebase = {
-  apiKey: "AIzaSyAyCfW-GM9ebbYj-VPEIA9ZGDhxidjttuI",
-  authDomain: "gerenciadordepresenca-9f4a4.firebaseapp.com",
-  projectId: "gerenciadordepresenca-9f4a4",
-  storageBucket: "gerenciadordepresenca-9f4a4.appspot.com",
-  messagingSenderId: "570789717517",
-  appId: "1:570789717517:web:aed02ab646a1bc23471744",
-  measurementId: "G-J7GX261L93"
+  apiKey: "AIzaSyCqFuOFHVVfGYfgwY8pJcKbAoICIVoxjUE",
+  authDomain: "gerenciador-de-faltas.firebaseapp.com",
+  projectId: "gerenciador-de-faltas",
+  storageBucket: "gerenciador-de-faltas.appspot.com",
+  messagingSenderId: "786608058590",
+  appId: "1:786608058590:web:4650103d719ad11b62124f"
 };
